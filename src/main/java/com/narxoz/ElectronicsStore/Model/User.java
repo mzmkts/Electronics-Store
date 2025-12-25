7260b4939d89f7ed3bbf273c905515cfe2445735
mzmkts <glemillov@gmail.com>
2025-12-25 20:33:45 +0500

    refactor: Deleted password length

--- src/main/java/com/narxoz/ElectronicsStore/Model/User.java
+++ src/main/java/com/narxoz/ElectronicsStore/Model/User.java
@@ -21,7 +21,7 @@ public class User {
     @Column(name = "username", nullable = false, length = 20)
     private String username;
 
-    @Column(nullable = false, length = 20)
+    @Column(nullable = false)
     private String password;
 
     @Column(nullable = false, unique = true)
